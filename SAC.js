f7bbedcc6babe14e31e6dc5ebacc8656fe66b00f
chieffancypants <chieffancypants@gmail.com>
2013-01-20 15:53:05 -0500

    Ignore network-related errors, and just try again later

--- SAC.js
+++ SAC.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var request = require('request'),
 	exec = require('child_process').exec,
 	fs = require('fs');
@@ -9,29 +10,30 @@ var pollFrequency = 1 * 60 * 1000,
 
 var poll = function() {
 	request.get('http://www.steepandcheap.com/steepcheap/sac/jsdata.js', function(err, resp, body) {
-		if (resp.statusCode == 200) {
+		if (!err && resp.statusCode == 200) {
 			body = JSON.parse(body);
 			
+			// Don't notify unless it's a new item
 			if (body.currentItem.skuClass === previousItem) {
 				return;
 			}
-
 			previousItem = body.currentItem.skuClass;
 
+			// Grab the first image
 			for (image in body.currentItem.variants) break;
 			var image = body.currentItem.variants[image].images.smallImage;
 			var r = request(image).pipe(fs.createWriteStream('data/image.jpg'));
 			
+			// Once we're done saving the image, display the notification:
 			r.on('close', function() {
 				var message = 'SteepAndCheap "' + body.currentItem.productTitle +
 					' - ' + body.currentItem.price +
-					' (' + body.currentItem.percentOff +'% off)" -i ' + __dirname + '/data/image.jpg';
-					
-				// console.log(message);
+					' (' + body.currentItem.percentOff +'% off)" -i ' + __dirname + '/data/image.jpg';					
 				exec('notify-send ' + message, function (err, stdout, stderr) {
 					// console.log(err, stdout, stderr);
 				});
-			})
+			});
+			
 		}
 	});
 }
