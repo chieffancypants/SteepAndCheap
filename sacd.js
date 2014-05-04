21f570368b012e21758dbead2bbfb3ff3d338616
Ian Young <ian.greenleaf@gmail.com>
2014-05-04 15:34:08 -0700

    Support checking multiple sites
    Allows checking of any combination of SteepAndCheap, Chainlove, and
    WhiskeyMilitia through the SITES env variable, as so:
    
        SITES=steepandcheap,chainlove ./sacd.js

--- sacd.js
+++ sacd.js
@@ -5,7 +5,7 @@ var request  = require('request'),
     growl    = require('growl'),
     util     = require('util');
 
-var previousItem,
+var previousItems = {},
   pollFrequency = 1 * 60 * 1000,
 
 // set header information:
@@ -27,29 +27,45 @@ process.on('uncaughtException', function (err) {
 });
 
 var poll = function() {
-  request.get('http://www.steepandcheap.com/steepcheap/sac/jsdata.js', function (err, resp, body) {
-    if (err) throw(err);
+  var endpoints = {
+    steepandcheap: {name: 'SteepAndCheap', url: 'http://www.steepandcheap.com/steepcheap/sac/jsdata.js' }
+    , chainlove: {name: 'Chainlove', url: 'http://www.chainlove.com/chainlove/cl/jsdata.js' }
+    , whiskeymilitia: {name: 'WhiskeyMilitia', url: 'http://www.whiskeymilitia.com/wm/wm/jsdata.js' }
+  };
 
-    if (!err && resp.statusCode == 200) {
-      // Don't notify unless it's a new item
-      if (body.currentItem.skuClass === previousItem) {
-        return;
-      }
-      previousItem = body.currentItem.skuClass;
+  var desiredSites;
+  if (process.env.SITES) {
+    desiredSites = process.env.SITES.split(',');
+  } else {
+    desiredSites = ['steepandcheap'];
+  }
 
-      // Grab the first image
-      for (var image in body.currentItem.variants) break;
-      image = body.currentItem.variants[image].images.smallImage;
-      var r = request(image).pipe(fs.createWriteStream(__dirname + '/data/image.jpg'));
+  desiredSites.forEach(function (site) {
+    var endpoint = endpoints[site].url;
+    request.get(endpoint, function (err, resp, body) {
+      if (err) throw(err);
 
-      // Once we're done saving the image, display the notification:
-      r.on('close', function() {
-        var message = body.currentItem.productTitle +
+      if (!err && resp.statusCode == 200) {
+        // Don't notify unless it's a new item
+        if (body.currentItem.skuClass === previousItems[site]) {
+          return;
+        }
+        previousItems[site] = body.currentItem.skuClass;
+
+        // Grab the first image
+        for (var image in body.currentItem.variants) break;
+        image = body.currentItem.variants[image].images.smallImage;
+        var r = request(image).pipe(fs.createWriteStream(__dirname + '/data/' + site + '.jpg'));
+
+        // Once we're done saving the image, display the notification:
+        r.on('close', function() {
+          var message = body.currentItem.productTitle +
           ' - ' + body.currentItem.price +
           ' (' + body.currentItem.percentOff +'% off)';
-        growl(message, {title: 'SteepAndCheap', image: __dirname + '/data/image.jpg'});
-      });
-    }
+        growl(message, {title: endpoints[site].name, image: __dirname + '/data/' + site + '.jpg'});
+        });
+      }
+    });
   });
 };
 
